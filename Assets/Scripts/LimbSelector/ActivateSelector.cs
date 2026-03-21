57c8701dc96c1ee9870f7e61660bb8a31b8bdabf
Mia Trujillo <mia.trujillo@uniat.edu.mx>
2026-03-20 20:23:59 -0600

    Menu Animation
    - Added DOTween Package
    - Added a Open and Close Animation for the LimbSelector

--- Assets/Scripts/LimbSelector/ActivateSelector.cs
+++ Assets/Scripts/LimbSelector/ActivateSelector.cs
@@ -1,25 +1,30 @@
 using UnityEngine;
-using MoreMountains.Feedbacks;
 
 public class ActivateSelector : MonoBehaviour
 {
     [SerializeField] private GameObject selectionMenu;
+    [SerializeField] private Selector selector;
+
+    private bool isOpen = false;
 
     private void Update()
     {
-        if (InputManager.Instance.Selection)
+        if (InputManager.Instance.Selection && !isOpen)
         {
             OpenSelectionMenu();
+            isOpen = true;
         }
-        else
+        else if (!InputManager.Instance.Selection && isOpen) 
         {
             CloseSelectionMenu();
+            isOpen = false;
         }
     }
 
     private void OpenSelectionMenu()
     {
         selectionMenu.SetActive(true);
+        selector.AnimateOpen();
 
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
@@ -27,9 +32,17 @@ public class ActivateSelector : MonoBehaviour
 
     private void CloseSelectionMenu()
     {
-        selectionMenu.SetActive(false);
+        selector.AnimateClose();
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+
+        CancelInvoke(nameof(CloseMenu));
+        Invoke(nameof(CloseMenu), 0.4f);
+    }
+
+    private void CloseMenu()
+    {
+        selectionMenu.SetActive(false);
     }
 }
